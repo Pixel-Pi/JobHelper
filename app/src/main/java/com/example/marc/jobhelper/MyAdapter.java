d20944131507ff2300274313ceaa1582484b7744
DJPixelPi <mf-153025@hs-weingarten.de>
2017-06-15 18:23:37 +0200

    Created a Relative Layout for showing entries of companies

---
+++ app/src/main/java/com/example/marc/jobhelper/MyAdapter.java
@@ -0,0 +1,112 @@
+package com.example.marc.jobhelper;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<CompanyItem> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView txtCompName;
+        public TextView txtJobTitle;
+        public TextView txtAppStatus;
+        public ImageView img;
+
+        public ViewHolder(View v) {
+            super(v);
+            txtCompName = (TextView) v.findViewById(R.id.company_name);
+            txtJobTitle = (TextView) v.findViewById(R.id.job_title);
+            txtAppStatus = (TextView) v.findViewById(R.id.application_status);
+            img = (ImageView) v.findViewById(R.id.image_company);
+        }
+    }
+
+    public void add(int position, CompanyItem item) {
+        mDataset.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(CompanyItem item) {
+        int position = mDataset.indexOf(item);
+        mDataset.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public static class CompanyItem {
+        private Bitmap img;
+        private String CompanyName;
+        private String JobTitle;
+        private String Status;
+
+        public CompanyItem(Bitmap _img, String _CompName, String _JobTitle, String _Status){
+            img = _img;
+            CompanyName = _CompName;
+            JobTitle = _JobTitle;
+            Status = _Status;
+        }
+
+
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(List<CompanyItem> myDataset) {
+        System.out.println(myDataset + "");
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.company_list_item, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final CompanyItem item = mDataset.get(position);
+        holder.txtCompName.setText(mDataset.get(position).CompanyName);
+        /*holder.txtJobTitle.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(name);
+            }
+        });*/
+        //TODO OnClickListener bei Konstruktor erstellen
+        //TODO Alle Listener in eigene Klassen
+        holder.txtJobTitle.setText(mDataset.get(position).JobTitle);
+        holder.txtAppStatus.setText(mDataset.get(position).Status);
+        if(mDataset.get(position).img != null) holder.img.setImageBitmap(mDataset.get(position).img);
+
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+}
--- app/src/main/java/com/example/marc/jobhelper/planned.java
+++ app/src/main/java/com/example/marc/jobhelper/planned.java
@@ -4,17 +4,27 @@ package com.example.marc.jobhelper;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.marc.jobhelper.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class planned extends Fragment {
+public class planned extends Fragment implements AdapterView.OnItemClickListener{
 
+   RecyclerView recyclerView;
 
     public planned() {
         // Required empty public constructor
@@ -25,7 +35,29 @@ public class planned extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planned, container, false);
+        View view = inflater.inflate(R.layout.fragment_planned, container, false);
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.list_planned);
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(container.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        List<MyAdapter.CompanyItem> myData = new ArrayList<>();
+        MyAdapter.CompanyItem tempItem;
+        for(int i = 0; i < 10; i++){
+            tempItem = new MyAdapter.CompanyItem(null, "Company "+ i, "JobTitle "+ i, "Geplant");
+            myData.add(tempItem);
+        }
+
+        MyAdapter adapter = new MyAdapter(myData);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
